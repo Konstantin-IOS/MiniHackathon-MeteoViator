adc9032136555492351722dd47b345f379ad60a3
Konstantin <67009085+Konstantin-IOS@users.noreply.github.com>
2024-07-14 18:33:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ import openmeteo_requests
 import requests_cache
 import numpy as np
 import altair as alt
-from geopy.geocoders import Nominatim
-from streamlit_extras.let_it_rain import rain
 
+# Fest vorgegebene Koordinaten für Freiburg im Breisgau
+latitude = 47.997791
+longitude = 7.842609
 
 col1, col2 = st.columns([3, 1])
 
@@ -33,114 +34,84 @@ end_date = st.date_input("Enddatum", value=datetime.now())
 start_date_str = start_date.strftime("%Y-%m-%d")
 end_date_str = end_date.strftime("%Y-%m-%d")
 
-# Geocoder initialisieren
-geolocator = Nominatim(user_agent="my_geocoder")
+# Direkt festgelegte Koordinaten verwenden
+latitude, longitude = 47.997791, 7.842609
 
-# Funktion zur Ermittlung der Koordinaten
-def get_coordinates(city, zip_code):
-    if city:
-        location = geolocator.geocode(f"{city}, Deutschland")
-    else:
-        location = None
-    
-    if not location and zip_code:
-        location = geolocator.geocode(f"Deutschland {zip_code}")
+url = "https://archive-api.open-meteo.com/v1/archive"
+params = {
+    "latitude": latitude,
+    "longitude": longitude,
+    "start_date": start_date_str,
+    "end_date": end_date_str,
+    "hourly": "temperature_2m,wind_speed_10m,relative_humidity_2m,sunshine_duration"
+}
 
-    if location:
-        return location.latitude, location.longitude
-    else:
-        return None, None
+cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
+retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+openmeteo = openmeteo_requests.Client(session=retry_session)
 
-# Koordinaten ermitteln
-latitude, longitude = get_coordinates(city, zip_code)
+if st.button("Wetterdaten abrufen"):
+    try:
+        # Regenanimation
+        st.write("Lade Wetterdaten...")
+        rain(emoji='🌤️', font_size=54, falling_speed=6, animation_length="1")
+        
+        # Wetterdaten abrufen
+        responses = openmeteo.weather_api(url, params=params)
+        response = responses[0]
+        
+        # Daten verarbeiten
+        hourly = response.Hourly()
+        hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+        hourly_wind_speed_10m = hourly.Variables(1).ValuesAsNumpy()
+        hourly_relative_humidity_2m = hourly.Variables(2).ValuesAsNumpy()
+        hourly_sunshine_duration = hourly.Variables(3).ValuesAsNumpy()
 
-if latitude is not None and longitude is not None:
-    
+        hourly_data = {
+            "date": pd.date_range(
+                start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+                end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+                freq=pd.Timedelta(seconds=hourly.Interval()),
+                inclusive="left"
+            ),
+            "temperature_2m": hourly_temperature_2m,
+            "wind_speed_10m": hourly_wind_speed_10m,
+            "relative_humidity_2m": hourly_relative_humidity_2m,
+            "sunshine_duration": hourly_sunshine_duration
+        }
 
-    
-    url = "https://archive-api.open-meteo.com/v1/archive"
-    params = {
-        "latitude": latitude,
-        "longitude": longitude,
-        "start_date": start_date_str,
-        "end_date": end_date_str,
-        "hourly": "temperature_2m,wind_speed_10m,relative_humidity_2m,sunshine_duration"
-    }
+        hourly_dataframe = pd.DataFrame(data=hourly_data)
 
-    
-    cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
-    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
-    openmeteo = openmeteo_requests.Client(session=retry_session)
+        # Durchschnittswerte berechnen
+        avg_temp = hourly_dataframe["temperature_2m"].mean()
+        avg_wind_speed = hourly_dataframe["wind_speed_10m"].mean()
+        avg_sunshine_duration_per_day = hourly_dataframe["sunshine_duration"].sum() / 3600
 
-    
-    if st.button("Wetterdaten abrufen"):
-        try:
-            rain(emoji='🌤️', font_size=54, falling_speed=6, animation_length="1")
+        col1, col2, col3 = st.columns(3)
+        col1.metric(label="Durchschnitts-Temperatur", value=f"{avg_temp:.2f} °C")
+        col2.metric(label="Durchschnitts-Windgeschwindigkeit", value=f"{avg_wind_speed:.2f} km/h")
+        col3.metric(label="Durchschnittliche Sonnenstunden", value=f"{avg_sunshine_duration_per_day:.2f} h/Tag")
+        st.divider()
+
+        # Tabellen- und Chart-Anzeige
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.write("#### Wetterdaten-Tabelle")
+            st.write(hourly_dataframe)
+
+        with col2:
+            st.write("#### Temperaturverlauf")
             
-            responses = openmeteo.weather_api(url, params=params)
+            # Altair Chart
+            chart = alt.Chart(hourly_dataframe).mark_line().encode(
+                x='date:T',
+                y=alt.Y('temperature_2m:Q', scale=alt.Scale(domain=[-40, 50]))
+            ).properties(
+                width=600,
+                height=425
+            )
+            st.altair_chart(chart, use_container_width=True)
 
-            
-            response = responses[0]
-
-            
-            hourly = response.Hourly()
-            hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-            hourly_wind_speed_10m = hourly.Variables(1).ValuesAsNumpy()
-            hourly_relative_humidity_2m = hourly.Variables(2).ValuesAsNumpy()
-            hourly_sunshine_duration = hourly.Variables(3).ValuesAsNumpy()
-
-            hourly_data = {
-                "date": pd.date_range(
-                    start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
-                    end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
-                    freq=pd.Timedelta(seconds=hourly.Interval()),
-                    inclusive="left"
-                ),
-                "temperature_2m": hourly_temperature_2m,
-                "wind_speed_10m": hourly_wind_speed_10m,
-                "relative_humidity_2m": hourly_relative_humidity_2m,
-                "sunshine_duration": hourly_sunshine_duration
-            }
-
-            hourly_dataframe = pd.DataFrame(data=hourly_data)
-            
-
-            # Durchschnittswerte und Änderungen berechnen
-            avg_temp = hourly_dataframe["temperature_2m"].mean()
-            avg_wind_speed = hourly_dataframe["wind_speed_10m"].mean()
-
-            # Sonnenstunden pro Tag berechnen
-            daily_sunshine = hourly_dataframe.groupby(hourly_dataframe['date'].dt.date)["sunshine_duration"].sum()
-            avg_sunshine_duration_per_day = daily_sunshine.mean()
-
-            col1, col2, col3 = st.columns(3)
-            col1.metric(label="Durchschnitts-Temperatur", value=f"{avg_temp:.2f} °C")
-            col2.metric(label="Durchschnitts-Windgeschwindigkeit", value=f"{avg_wind_speed:.2f} km/h")
-            col3.metric(label="Durchschnittliche Sonnenstunden", value=f"{avg_sunshine_duration_per_day / 3600:.2f} h/Tag")
-            st.divider()
-            # Layout mit zwei Spalten auf gleicher Höhe anordnen
-            
-            col1, col2 = st.columns(2)
-
-            with col1:
-                st.write("#### Wetterdaten-Tabelle")
-                st.write(hourly_dataframe)
-                
-
-            with col2:
-                st.write("#### Temperaturverlauf")
-                
-                # Altair Chart erstellen
-                chart = alt.Chart(hourly_dataframe).mark_line().encode(
-                    x='date:T',
-                    y=alt.Y('temperature_2m:Q', scale=alt.Scale(domain=[-40, 50]))
-                ).properties(
-                    width=600,
-                    height=425
-                )
-                st.altair_chart(chart, use_container_width=True)
-
-        except Exception as e:
-            st.error(f"Ein Fehler ist aufgetreten: {e}")
-else:
-    st.error("Koordinaten konnten nicht ermittelt werden. Bitte überprüfe die Eingaben für Stadt und Postleitzahl.")
+    except Exception as e:
+        st.error(f"Ein Fehler ist aufgetreten: {e}")
